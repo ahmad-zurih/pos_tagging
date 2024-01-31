46c6edc8634f6aa9de9ffbd8e6f576b817500d5a
Ahmad Alhineidi <ahmadhineidi@hotmail.com>
2024-01-31 16:27:43 +0100

    fix 3

--- pages/short_text.py
+++ pages/short_text.py
@@ -1,19 +1,29 @@
+import dill
 import streamlit as st
 from nltk.tokenize import word_tokenize
-import dill
+from io import StringIO
+import nltk
 
 nltk.download('punkt')
 
+st.set_page_config(
+        page_title="POS Tagger - Tag short text",
+)
+
+
+# Load the trained model from the file
 with open('hmm_tagger.pkl', 'rb') as f:
     loaded_tagger = dill.load(f)
 
-short_text = st.text_input("Please insert a sentence to get the POS tag for it: ")
 
-if short_text:
-    tokens = word_tokenize(short_text)
-    tagged = loaded_tagger.tag(tokens)
-    html_result = ""
-    for word, pos in tagged:
-        html_result += f"<span style='color: blue;'>{word} </span> <span style='color: red;'> {pos} </span>"
-    st.markdown(html_result, unsafe_allow_html=True)
+text = st.text_input("Insert a text to get the POS tagging")
+
+if text:
+    tokens = word_tokenize(text)
+    tagged_sentence = loaded_tagger.tag(tokens)
+    results_html = ""
+    for word, tag in tagged_sentence:
+        results_html += f"<span style='color: blue;'>{word}</span> <span style='color: red;'>{tag}</span> "
+
+    st.markdown(results_html, unsafe_allow_html=True)
     
